d269ea00a4b80b88132f7b96a678607f75ee39e6
shreeshh <tripathi.shreeshv@gmail.com>
2019-07-03 18:31:49 +0530

    almost done

---
+++ SRIP/Codes/html/bones/chart.js
@@ -0,0 +1,477 @@
+var t1= [
+[2,22,35,38,52,57,64,69,70,76,82,89,96,109,115,119,120,122,157,170,193,207,210,211,217,4,7,9,15,24,25,41,60,68,71,73,78,79,100,103,105,106,113,114,125,129,135,153,175,178],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0.190,0.204,0.216,0.237,0.279,0.277,0.300,0.148,0.273,0.202,0.231,0.201,0.242,0.161,0.269,0.153,0.281,0.285,0.161,0.246,0.184,0.239,0.235,0.100,0.174,0.092,0.093,0.061,0.055,0.089,0.123,0.081,0.097,0.074,0.123,0.057,0.097,0.107,0.068,0.052,0.059,0.112,0.074,0.118,0.052,0.109,0.141,0.078,0.057,0.117],
+[0.092,0.095,0.085,0.090,0.097,0.095,0.100,0.100,0.087,0.090,0.090,0.095,0.085,0.100,0.108,0.090,0.100,0.100,0.117,0.090,0.090,0.100,0.100,0.085,0.087,0.085,0.070,0.062,0.062,0.085,0.090,0.065,0.082,0.075,0.080,0.060,0.077,0.080,0.080,0.060,0.067,0.080,0.065,0.082,0.065,0.067,0.090,0.070,0.065,0.077],
+[87,86,77,73,93,90,94,103,77,77,84,92,79,99,86,91,87,105,92,76,82,101,95,87,84,50,49,46,47,46,77,46,48,73,62,46,49,49,47,45,46,51,44,48,45,49,52,50,46,51],
+[131,143,113,132,149,150,154,72,125,127,135,110,107,112,136,108,166,111,120,105,98,140,110,92,125,86,52,39,28,86,104,50,85,71,74,28,75,88,72,31,53,81,34,79,48,48,107,58,51,77],
+[37,133,119,130,125,139,160,96,116,128,127,109,108,124,132,116,160,142,123,107,115,104,102,90,104,84,69,45,33,85,100,45,83,79,72,40,75,91,74,38,53,84,53,87,51,62,108,58,47,82,],
+[2,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+	]
+
+var t2 = [
+[6,17,26,29,77,83,110,118,121,123,144,160,162,172,179,181,188,190,191,200,214,221,234,253,254,8,11,13,28,31,45,50,51,75,87,99,108,112,131,136,137,150,158,180,182,199,204,208,216,229],
+[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+[0.168,0.151,0.248,0.260,0.143,0.246,0.126,0.256,0.244,0.140,0.155,0.179,0.214,0.216,0.155,0.098,0.252,0.206,0.184,0.167,0.242,0.223,0.183,0.174,0.229,0.223,0.163,0.225,0.300,0.155,0.098,0.186,0.106,0.153,0.120,0.118,0.186,0.292,0.126,0.201,0.104,0.219,0.195,0.223,0.206,0.160,0.184,0.161,0.204,0.103],
+[0.117,0.117,0.100,0.100,0.100,0.092,0.173,0.117,0.100,0.090,0.155,0.140,0.100,0.117,0.095,0.075,0.100,0.140,0.095,0.173,0.100,0.100,0.095,0.117,0.100,0.095,0.085,0.100,0.100,0.140,0.085,0.100,0.085,0.082,0.100,0.080,0.095,0.100,0.085,0.100,0.085,0.092,0.100,0.108,0.100,0.092,0.108,0.117,0.095,0.092],
+[82,88,98,87,84,83,75,97,79,73,83,92,97,85,97,58,89,81,81,82,86,109,81,80,94,92,67,94,90,78,72,83,84,73,81,75,87,113,69,90,75,100,88,105,97,83,89,92,86,73],
+[135,107,120,142,108,124,101,123,117,114,100,115,129,120,108,67,153,121,124,96,153,140,156,117,181,134,76,147,124,113,86,123,89,104,132,99,103,182,88,122,100,120,119,142,155,115,144,133,112,113,],
+[128,108,125,134,110,101,83,111,139,88,100,108,133,114,103,68,121,92,106,94,128,134,136,111,149,134,97,127,129,109,79,130,82,89,115,83,124,134,99,135,84,114,137,137,139,122,140,116,135,110],
+[2,3,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,2,1,2,1,2,1,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+
+var t3 = [
+[18,32,42,47,56,86,95,98,126,138,145,147,161,189,198,203,226,241,246,266,296,301,313,334,344,5,20,23,34,44,46,49,55,58,81,88,111,117,134,154,155,163,168,171,173,177,184,196,227],
+[8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+[0.163,0.195,0.175,0.126,0.201,0.156,0.156,0.168,0.155,0.174,0.107,0.172,0.126,0.165,0.143,0.153,0.201,0.110,0.231,0.219,0.150,0.131,0.191,0.197,0.161,0.131,0.098,0.083,0.117,0.100,0.143,0.092,0.131,0.120,0.151,0.155,0.145,0.155,0.106,0.089,0.125,0.120,0.153,0.120,0.143,0.112,0.156,0.131,0.136],
+[0.090,0.097,0.085,0.085,0.095,0.095,0.085,0.090,0.090,0.082,0.085,0.097,0.085,0.082,0.095,0.097,0.092,0.080,0.090,0.090,0.090,0.080,0.100,0.090,0.095,0.087,0.085,0.080,0.092,0.077,0.085,0.075,0.095,0.080,0.085,0.087,0.085,0.082,0.075,0.080,0.092,0.080,0.085,0.082,0.095,0.077,0.085,0.085,0.082],
+[62,69,67,82,75,86,67,75,74,54,89,82,70,86,77,70,81,65,79,75,78,58,80,94,76,69,57,64,57,59,72,64,83,66,92,64,73,67,56,65,86,70,70,71,72,59,73,68,70],
+[116,141,103,115,143,127,118,130,107,76,96,132,90,118,121,127,141,83,118,136,120,91,127,119,136,102,84,83,96,83,102,96,116,92,100,109,90,87,79,84,118,95,110,89,116,86,85,102,93],
+[117,141,104,86,127,123,109,120,95,85,102,128,90,101,122,120,144,82,115,135,124,81,124,105,131,102,85,83,105,78,97,82,94,82,109,104,96,93,75,74,104,84,105,88,101,88,68,103,92],
+[3,3,3,3,3,3,3,3,2,3,3,2,3,2,4,3,2,3,3,3,3,2,3,3,2,2,2,2,2,2,2,1,2,3,3,2,2,2,2,1,2,2,2,2,2,2,2,2]
+]
+
+function inst()
+{
+	alert("Please choose a pair of feature from the drop down menu below");
+}
+
+function crar1() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[2];
+	const ys = data[3];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"Contrast Ratio"
+	},
+	axisY:{
+		title: "Aspect Ratio"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Contrast Ratio:</b> {x} <br/><b> Aspect Ratio:</b></span> {y} ",
+		name: "Digit Class: '1'",
+		showInLegend: true,
+		dataPoints: parseData(t1,1)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Contrast Ratio:</b> {x} <br/><b> Aspect Ratio:</b></span> {y} ",
+		name: "Digit Class: '0'",
+		showInLegend: true,
+		dataPoints: parseData(t1,0)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+function hvp1() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[7];
+	const ys = data[4];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"Holes"
+	},
+	axisY:{
+		title: "Perimeter"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Holes:</b> {x} <br/><b> Perimeter:</b></span> {y} ",
+		name: "Digit Class: '1'",
+		showInLegend: true,
+		dataPoints: parseData(t1,1)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Holes:</b> {x} <br/><b> Perimeter:</b></span> {y} ",
+		name: "Digit Class: '0'",
+		showInLegend: true,
+		dataPoints: parseData(t1,0)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+function xvy1() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[5];
+	const ys = data[6];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"X-Symmetry"
+	},
+	axisY:{
+		title: "Y-Symmetry"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> X-Symmetry:</b> {x} <br/><b> Y-Symmetry:</b></span> {y} ",
+		name: "Digit Class: '1'",
+		showInLegend: true,
+		dataPoints: parseData(t1,1)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> X-Symmetry:</b> {x} <br/><b> Y-Symmetry:</b></span> {y} ",
+		name: "Digit Class: '0'",
+		showInLegend: true,
+		dataPoints: parseData(t1,0)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function crar2() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[2];
+	const ys = data[3];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"Contrast Ratio"
+	},
+	axisY:{
+		title: "Aspect Ratio"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Contrast Ratio:</b> {x} <br/><b> Aspect Ratio:</b></span> {y} ",
+		name: "Digit Class: '2'",
+		showInLegend: true,
+		dataPoints: parseData(t2,2)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Contrast Ratio:</b> {x} <br/><b> Aspect Ratio:</b></span> {y} ",
+		name: "Digit Class: '3'",
+		showInLegend: true,
+		dataPoints: parseData(t2,3)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+function hvp2() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[7];
+	const ys = data[4];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"Holes"
+	},
+	axisY:{
+		title: "Perimeter"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Holes:</b> {x} <br/><b> Perimeter:</b></span> {y} ",
+		name: "Digit Class: '2'",
+		showInLegend: true,
+		dataPoints: parseData(t2,2)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Holes:</b> {x} <br/><b> Perimeter:</b></span> {y} ",
+		name: "Digit Class: '3'",
+		showInLegend: true,
+		dataPoints: parseData(t2,3)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+function xvy2() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[5];
+	const ys = data[6];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"X-Symmetry"
+	},
+	axisY:{
+		title: "Y-Symmetry"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> X-Symmetry:</b> {x} <br/><b> Y-Symmetry:</b></span> {y} ",
+		name: "Digit Class: '2'",
+		showInLegend: true,
+		dataPoints: parseData(t2,2)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> X-Symmetry:</b> {x} <br/><b> Y-Symmetry:</b></span> {y} ",
+		name: "Digit Class: '3'",
+		showInLegend: true,
+		dataPoints: parseData(t2,3)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function crar3() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[2];
+	const ys = data[3];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"Contrast Ratio"
+	},
+	axisY:{
+		title: "Aspect Ratio"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Contrast Ratio:</b> {x} <br/><b> Aspect Ratio:</b></span> {y} ",
+		name: "Digit Class: '8'",
+		showInLegend: true,
+		dataPoints: parseData(t3,8)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Contrast Ratio:</b> {x} <br/><b> Aspect Ratio:</b></span> {y} ",
+		name: "Digit Class: '9'",
+		showInLegend: true,
+		dataPoints: parseData(t3,9)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+function hvp3() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[7];
+	const ys = data[4];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"Holes"
+	},
+	axisY:{
+		title: "Perimeter"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Holes:</b> {x} <br/><b> Perimeter:</b></span> {y} ",
+		name: "Digit Class: '8'",
+		showInLegend: true,
+		dataPoints: parseData(t3,8)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> Holes:</b> {x} <br/><b> Perimeter:</b></span> {y} ",
+		name: "Digit Class: '9'",
+		showInLegend: true,
+		dataPoints: parseData(t3,9)
+	}
+	]
+});
+
+chart.render();
+
+}
+
+function xvy3() {
+
+
+const zip = (a,b) => a.map((e,i) => ([e, b[i]]));
+const parseData = (data, cls) => {
+	const is = data[1];
+	const xs = data[5];
+	const ys = data[6];
+
+	const classXs = xs.filter((x,i) => is[i] == cls);
+	const classYs = ys.filter((y,i) => is[i] == cls);
+
+	const chartData = zip(classXs, classYs).map(([x,y]) => ({x, y}))
+	return chartData;
+
+}
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Feature Representation"
+	},
+	axisX: {
+		title:"X-Symmetry"
+	},
+	axisY:{
+		title: "Y-Symmetry"
+	},
+	data: [{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> X-Symmetry:</b> {x} <br/><b> Y-Symmetry:</b></span> {y} ",
+		name: "Digit Class: '8'",
+		showInLegend: true,
+		dataPoints: parseData(t3,8)
+	},
+	{
+		type: "scatter",
+		toolTipContent: "<span style=\"color:#C0504E \"><b>{name}</b></span><br/><b> X-Symmetry:</b> {x} <br/><b> Y-Symmetry:</b></span> {y} ",
+		name: "Digit Class: '9'",
+		showInLegend: true,
+		dataPoints: parseData(t3,9)
+	}
+	]
+});
+
+chart.render();
+
+}	
